a644a6fbc4cd43c834f72d055e8bd2b2916440a7
Derek Honeycutt <derekshoneycutt@gmail.com>
2026-02-22 01:58:09 -0800

    Some cleanup

--- numeric/primes500/primes.hs
+++ numeric/primes500/primes.hs
@@ -3,8 +3,8 @@ import Text.Printf
 primes :: [Integer]
 primes = sieve [2..]
   where
-    sieve (p : xs) = p : sieve [x | x <- xs,
-                                   x `mod` p /= 0]
+    sieve (p : xs) =
+        p : sieve [x | x <- xs, x `mod` p /= 0]
     sieve [] = []
 
 firstPrimes :: Int -> [Integer]
@@ -13,19 +13,23 @@ firstPrimes n = take n primes
 pad :: Integer -> String
 pad num = printf "%04d" num
 
+printLine :: [Integer] -> IO ()
+printLine values =
+    putStrLn $ "     "
+        ++ pad (values !! 0) ++ " " ++ pad (values !! 1)
+        ++ " " ++ pad (values !! 2) ++ " " ++ pad (values !! 3)
+        ++ " " ++ pad (values !! 4) ++ " " ++ pad (values !! 5)
+        ++ " " ++ pad (values !! 6) ++ " " ++ pad (values !! 7)
+        ++ " " ++ pad (values !! 8) ++ " " ++ pad (values !! 9)
+
 printLines :: [Integer] -> Int -> IO ()
 printLines values i
     | (length values) `div` 10 > i = do
-        putStrLn $ "     " ++ pad (values !! i)
-            ++ " " ++ pad (values !! (i + 50))
-            ++ " " ++ pad (values !! (i + 100))
-            ++ " " ++ pad (values !! (i + 150))
-            ++ " " ++ pad (values !! (i + 200))
-            ++ " " ++ pad (values !! (i + 250))
-            ++ " " ++ pad (values !! (i + 300))
-            ++ " " ++ pad (values !! (i + 350))
-            ++ " " ++ pad (values !! (i + 400))
-            ++ " " ++ pad (values !! (i + 450))
+        printLine [(values !! i), (values !! (i + 50)),
+            (values !! (i + 100)), (values !! (i + 150)),
+            (values !! (i + 200)), (values !! (i + 250)),
+            (values !! (i + 300)), (values !! (i + 350)),
+            (values !! (i + 400)), (values !! (i + 450))]
         printLines values (i + 1)
     | otherwise = return ()
 
