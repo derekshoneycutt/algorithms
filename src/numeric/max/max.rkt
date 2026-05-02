35a90387678e32792b8e1e5524ddb7715d9ccb15
Derek Honeycutt <derekshoneycutt@gmail.com>
2026-05-02 10:42:57 -0700

    Racket Max

---
+++ src/numeric/max/max.rkt
@@ -0,0 +1,25 @@
+#|
+ |  This application gets the maximum value of a sequence of values
+ |#
+#lang racket
+
+; Find the maximum value in a list
+(define (mymax list)
+    (foldl (lambda (next curr)
+                (if (> next curr)
+                    next
+                    curr))
+          0 list))
+
+; Get the command line arguments as integers
+(define (argsAsInts args default)
+    (if (> (length args) 0)
+        (map (lambda (val) (string->number val)) args)
+        default))
+
+; Get the values and the max, and print the values
+(let* ((args (vector->list (current-command-line-arguments)))
+       (list (argsAsInts (cdr args) '(15 10)))
+       (max_value (mymax list)))
+    (display "values: ") (display list)
+    (display "\nmax: ") (display max_value) (display "\n"))
